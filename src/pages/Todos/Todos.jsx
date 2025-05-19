aa761044c159dad4380c245ea4cd74ad6a7b736e
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-05-19 19:48:14 +0300

    react

--- src/pages/Todos/Todos.jsx
+++ src/pages/Todos/Todos.jsx
@@ -2,17 +2,6 @@ import React from "./Todos.module.scss";
 
 const Todos = () => {
   return <div>Todos</div>;
-  <section>
-    <h2>Todos</h2>
-    <ul>
-      {data.map((item, idx) => (
-        <li key={idx}>
-          <p>{item.todo}</p>
-          <button>Remove</button>
-        </li>
-      ))}
-    </ul>
-  </section>;
 };
 
 export default Todos;
