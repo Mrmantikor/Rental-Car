d82f6cc56028be24777d2c3a5ac26fe3aecd6308
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-05-25 15:36:07 +0300

    react

--- src/components/CarElementRight/specifications/specifications.jsx
+++ src/components/CarElementRight/specifications/specifications.jsx
@@ -1,25 +1,29 @@
 import { Icon } from "../../../assets/Icon/Icon";
-import s from "./specifications.module.css";
+import s from "./specifications.module.scss";
 
 const Specifications = ({ year, type, fuelConsumption, engineSize }) => {
   return (
-    <div className={s.specifications_box}>
-      <p>Car Specifications:</p>
-      <div className={s.year}>
-        <Icon className={s["new"]} name="calendar" />
-        <p>Year: {year}</p>
+    <div className={s["specifications-box"]}>
+      <p className={s["title"]}>Car Specifications:</p>
+
+      <div className={s["spec-item"]}>
+        <Icon className={s["icon-calendar"]} name="calendar" />
+        <p className={s["label"]}>Year: {year}</p>
       </div>
-      <div className={s.type}>
-        <Icon className={s["car"]} name="car" />
-        <p>Type: {type}</p>
+
+      <div className={s["spec-item"]}>
+        <Icon className={s["icon-car"]} name="car" />
+        <p className={s["label"]}>Type: {type}</p>
       </div>
-      <div className={s.consumption}>
-        <Icon className={s["fuel"]} name="fuel" />
-        <p>Fuel consumption: {fuelConsumption}</p>
+
+      <div className={s["spec-item"]}>
+        <Icon className={s["icon-fuel"]} name="fuel" />
+        <p className={s["label"]}>Fuel consumption: {fuelConsumption}</p>
       </div>
-      <div className={s.engine_size}>
-        <Icon className={s["gear"]} name="gear" />
-        <p>Engine size: {engineSize}</p>
+
+      <div className={s["spec-item"]}>
+        <Icon className={s["icon-gear"]} name="gear" />
+        <p className={s["label"]}>Engine size: {engineSize}</p>
       </div>
     </div>
   );
